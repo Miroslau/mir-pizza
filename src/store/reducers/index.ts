016caf0142b26e54ec4f4202d0b2df6871290b45
Miroslav <mira2408@mail.ru>
2022-09-01 14:44:42 +0300

    feature: basket logic has been added

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import pizzaReducer from "./pizza-reducer";
 import filterReducer from "./filter-reducer";
+import cartReducer from "./cart-reducer";
 
 const rootReducer = combineReducers({
   pizza: pizzaReducer,
   filters: filterReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
